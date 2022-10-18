a2085ac46f7e3b55bfa802273d3a13b900b0274e
Luan K. Alves <luankauan10@gmail.com>
2022-10-18 18:15:29 -0300

    Update dollartoReal.py

--- tasks/dollartoReal.py
+++ tasks/dollartoReal.py
@@ -15,24 +15,29 @@ class Doll(commands.Cog):
     @tasks.loop(hours=1)
     async def dollar_to_real(self):
         now = datetime.datetime.now()
-        #A CONDICIONAL FAZ COM QUE ELE NAO RODE NOS FINAIS DE SEMANA!
-        if ( now.weekday() != 5 or now.weekday() != 6):
-            try:    
-                response = requests.get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-                data = response.json()
+    @tasks.loop(hours=1)
+    async def dollar_to_real(self):
+        now = datetime.datetime.now()
+        #NÃO RODA NOS FINAIS DE SEMANA!
+        if ( now.weekday() != 5 and now.weekday() != 6):
+            #RODA APENAS DE 9:00 AS 18 (GMT-3 & UTC−3)
+            if ( now.hour <= 9 and now.hour >= 18):
+                try:    
+                    response = requests.get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+                    data = response.json()
+                    
+
+                    cotacao_dolar = data["USDBRL"]["bid"]
+                    
+                    timeNow = now.strftime("%d/%m/%Y ás %H:%M:%S")
+
+                    channel = self.bot.get_channel(1027341105123041280)
+                    await channel.send(f"\n```━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n💸 Dólar : R${cotacao_dolar}\n\n📅 Em: {timeNow}  \n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n(próxima busca em 1 hora)\n```")
                 
 
-                cotacao_dolar = data["USDBRL"]["bid"]
-                
-                timeNow = now.strftime("%d/%m/%Y ás %H:%M:%S")
-
-                channel = self.bot.get_channel(1027341105123041280)
-                await channel.send(f"\n ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n💸 Dólar : R${cotacao_dolar}\n\n📅 Em: {timeNow}  \n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n(próxima busca em 1 hora)\n ")
-            
-
-            except Exception as error:
-                await channel.send("Ops... Ocorreu algum problema na consulta )")
-                print(error)
+                except Exception as error:
+                    await channel.send("Ops... Ocorreu algum problema na consulta )")
+                    print(error)
 
 
 
